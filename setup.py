78128705adf1d3417feafd4be3c66efc12cc4b91
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-07-19 08:40:38 +0200

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='collective.volto.cookieconsent',
-    version='1.1.1',
+    version='1.1.2.dev0',
     description="Add-on for Volto to manage cookieconsent banners",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
